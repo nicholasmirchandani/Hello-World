7ae8a696d6b752a5acf3bb2bbd3ddbc9f0c8f16d
nicholasmirchandani <44598785+nicholasmirchandani@users.noreply.github.com>
2019-09-04 14:25:55 -0700

    Update HelloWorld.cpp

--- HelloWorld.cpp
+++ HelloWorld.cpp
@@ -3,7 +3,7 @@ Name: Nicholas Mirchandani
 ID: 2317024
 Email: nmirchandani@chapman.edu
 Course: CPSC350-02
-Assignment 0: Hello World
+Assignment: Hello World
 */
 #include <iostream>
 int main() {
