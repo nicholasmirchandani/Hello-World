4f7d2876192eb39f8b7bfcf9afd1ac822b7588a5
Nicholas Mirchandani <nmirchandani@chapman.edu>
2019-09-04 17:59:18 +0000

    Adds Source Code

---
+++ HelloWorld.cpp
@@ -0,0 +1,12 @@
+/*
+Name: Nicholas Mirchandani
+ID: 2317024
+Email: nmirchandani@chapman.edu
+Course: CPSC350-02
+Assignment 0: Hello World
+*/
+#include <iostream>
+int main() {
+    std::cout << "Hello World!" << std::endl;
+    return 0;
+}
